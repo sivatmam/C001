f2963afca859ead6eb745911b389dda6f0aeb39f
Sivatma Maharaj <32339882+sivatmam@users.noreply.github.com>
2021-01-07 08:33:36 -0500

    People on the bus

---
+++ PeopleOnBus.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int change(int people, int total);
+
+int main(void){
+
+  int numPeople = 5;
+
+
+  printf("You are driving a bus.\n");
+  printf("The bus starts with %d people.\n",numPeople);
+  
+  numPeople = change(3,numPeople);
+  numPeople = change(-2,numPeople);
+  numPeople = change(-4,numPeople);
+  numPeople = change(5,numPeople);
+  numPeople = change(4,numPeople);
+}
+
+int change(int people, int total){
+
+  total += people;
+  if (people >= 0){
+    printf("%d People get on -> Bus has %d people.\n",people, total);   
+  } else {
+    printf("%d People get off -> Bus has %d people.\n",people, total);   
+  }
+  return total;
+
+}
